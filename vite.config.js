bcb91e0050286b5dc07eb8df018bb0ce9a7adb48
Afsheen Jahan S <afsheenj2k@gmail.com>
2026-04-01 12:14:54 +0530

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    allowedHosts: ['v-serve-app.onrender.com']
+  }
 })
