2d720eb5daded9652f7ce2240ead2a5defc1933d
Afsheen Jahan S <afsheenj2k@gmail.com>
2026-06-14 12:44:07 +0530

    added the local path and tried

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 export const APP_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || "https://v-serve-backend.onrender.com/api"
+  BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8080/api"
 };
